33742363dd00629f51234f852214844354e3b6f5
MarcGiroPiraces <marc.giro.piraces@gmail.com>
2022-03-13 16:47:46 +0100

    moviesRouter createMovie added and tested

--- src/server/routers/moviesRouter.js
+++ src/server/routers/moviesRouter.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { getMovies, deleteMovie } = require("../controllers/moviesController");
+const {
+  getMovies,
+  deleteMovie,
+  createMovie,
+} = require("../controllers/moviesController");
 
 const router = express.Router();
 
 router.get("/", getMovies);
 
 router.delete("/:movieId", deleteMovie);
+
+router.post("/", createMovie);
 
 module.exports = router;
--- src/server/routers/moviesRouter.test.js
+++ src/server/routers/moviesRouter.test.js
@@ -86,11 +86,60 @@ describe("Given a moviesRouter", () => {
       expect(body).toHaveProperty("message");
     });
   });
+
   describe("When it receives a delete request at the wrong endpoint", () => {
     test("Then it should respond with status 200", async () => {
       const { body } = await request(app).delete(`/movies/1234ljk`).expect(404);
 
       expect(body).toHaveProperty("message");
+    });
+  });
+
+  describe("When it receives a post request with the right body", () => {
+    test("Then it should respond with status 201 and the message 'Movie created'", async () => {
+      const message = "Movie created";
+
+      const { body } = await request(app)
+        .post(`/movies/`)
+        .send({
+          Title: "Adeu",
+          Type: "movie",
+          Actors: "protagonist supporting character",
+          Director: "movie director",
+          Genre: "drama",
+          Poster:
+            "https://m.media-amazon.com/images/M/MV5BNTE3MDc1MjY4NV5BMl5BanBnXkFtZTgwMDg4MjQ4MTE@._V1_SX300.jpg",
+          Plot: "summary of the movie",
+          Runtime: 120,
+          Writer: "movie writers",
+          Year: "1999",
+        })
+        .expect(201);
+
+      expect(body.message).toBe(message);
+    });
+  });
+
+  describe("When it receives a post request with the wrong body", () => {
+    test("Then it should respond with status 400 and the message 'We couldn't create the movie'", async () => {
+      const message = "We couldn't create the movie";
+      const { body } = await request(app)
+        .post(`/movies/`)
+        .send({
+          Type: "movie",
+          Actors: "protagonist supporting character",
+          Director: "movie director",
+          Genre: "drama",
+          Poster:
+            "https://m.media-amazon.com/images/M/MV5BNTE3MDc1MjY4NV5BMl5BanBnXkFtZTgwMDg4MjQ4MTE@._V1_SX300.jpg",
+          Plot: "summary of the movie",
+          Runtime: 120,
+          Writer: "movie writers",
+          Year: "1999",
+        })
+        .expect(400);
+
+      expect(body.message).toBe(message);
     });
   });
 });
