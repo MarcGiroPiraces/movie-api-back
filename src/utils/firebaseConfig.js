afa18102f2e4d318a6fb4c15178e7be809ce489e
MarcGiroPiraces <marc.giro.piraces@gmail.com>
2022-03-14 10:59:31 +0100

    firebase config added

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: "marcgiro-movieapi.firebaseapp.com",
+  projectId: "marcgiro-movieapi",
+  storageBucket: "marcgiro-movieapi.appspot.com",
+  messagingSenderId: "1038479901218",
+  appId: "1:1038479901218:web:20f458b1e5f545de97007b",
+};
+
+export default firebaseConfig;
