3db002f6e441c7a12d6cebfe0000bbd6292be56a
MarcGiroPiraces <marc.giro.piraces@gmail.com>
2022-03-16 17:38:08 +0100

    moviesRouter tested with auth

--- src/server/routers/moviesRouter.test.js
+++ src/server/routers/moviesRouter.test.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
 const request = require("supertest");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 const connectToMongoDB = require("../../database");
 const app = require("../index");
 const Movie = require("../../database/models/Movie");
+const User = require("../../database/models/user");
 
+let token;
 let database;
 beforeAll(async () => {
   database = await MongoMemoryServer.create();
@@ -18,6 +22,26 @@ afterAll(async () => {
 });
 
 beforeEach(async () => {
+  await User.create({
+    name: "name",
+    username: "user1",
+    password: "$2b$10$vQcjA2ldvcvUuGTil.Jp6uLgNoAZvVtmFFR1hHH4iKHz4zqfvl7oe",
+    movies: {},
+  });
+
+  const userDataToken = {
+    username: "user1",
+  };
+
+  token = jwt.sign(userDataToken, process.env.JWT_SECRET);
+
+  await User.create({
+    name: "user2",
+    username: "user2",
+    password: "user2",
+    movies: {},
+  });
+
   Movie.create({
     Title: "Hola",
     Type: "movie",
@@ -81,7 +105,10 @@ describe("Given a moviesRouter", () => {
       const movies = await request(app).get("/movies?s=Hola");
       const { id } = movies.body[0];
 
-      const { body } = await request(app).delete(`/movies/${id}`).expect(200);
+      const { body } = await request(app)
+        .delete(`/movies/${id}`)
+        .set("authorization", `Bearer ${token}`)
+        .expect(200);
 
       expect(body).toHaveProperty("message");
     });
@@ -89,32 +116,12 @@ describe("Given a moviesRouter", () => {
 
   describe("When it receives a delete request at the wrong endpoint", () => {
     test("Then it should respond with status 200", async () => {
-      const { body } = await request(app).delete(`/movies/1234ljk`).expect(404);
+      const { body } = await request(app)
+        .delete(`/movies/1234ljk`)
+        .set("authorization", `Bearer ${token}`)
+        .expect(404);
 
       expect(body).toHaveProperty("message");
     });
   });
-
-  // describe("When it receives a post request with the wrong body", () => {
-  //   test("Then it should respond with status 400 and the message 'We couldn't create the movie'", async () => {
-  //     const message = "Validation joi";
-  //     const { body } = await request(app)
-  //       .post(`/movies/`)
-  //       .send({
-  //         Type: "movie",
-  //         Actors: "protagonist supporting character",
-  //         Director: "movie director",
-  //         Genre: "drama",
-  //         Poster:
-  //           "https://m.media-amazon.com/images/M/MV5BNTE3MDc1MjY4NV5BMl5BanBnXkFtZTgwMDg4MjQ4MTE@._V1_SX300.jpg",
-  //         Plot: "summary of the movie",
-  //         Runtime: 120,
-  //         Writer: "movie writers",
-  //         Year: "1999",
-  //       })
-  //       .expect(401);
-
-  //     expect(body.message).toBe(message);
-  //   });
-  // });
 });
