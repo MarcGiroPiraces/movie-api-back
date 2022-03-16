0d180887afb23ad9f78065e15bef4ae7c810a550
MarcGiroPiraces <marc.giro.piraces@gmail.com>
2022-03-16 21:05:37 +0100

    added register to userRouter

--- src/server/routers/userRouter.js
+++ src/server/routers/userRouter.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { loginUser, loadUser } = require("../controllers/userController");
+const {
+  loginUser,
+  loadUser,
+  createUser,
+} = require("../controllers/userController");
 const auth = require("../middlewares/auth");
 
 const router = express.Router();
 
+router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/load-user", auth, loadUser);
 
